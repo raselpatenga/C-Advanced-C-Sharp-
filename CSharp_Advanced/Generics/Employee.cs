d2ddcf83f2b68067cbb3ef84e6204e0e1709f193
RASEL <raselpatenga@gmail.com>
2020-10-03 18:00:38 +0600

    Generics

---
+++ CSharp_Advanced/Generics/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Employee
+    {
+        public static Type showArray <Type> (Type firstInput, Type secondInput)
+        {
+            dynamic data1 = firstInput, data2 = secondInput;
+            return data1 + data2;
+        }
+    }
+}
---
+++ CSharp_Advanced/Generics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            string[] inputs = Console.ReadLine().Split(' ');
+            Console.WriteLine(Employee.showArray(numbers[0], numbers[1]));
+            Console.WriteLine(Employee.showArray(inputs[0], inputs[1]));
+            Console.ReadKey();
+        }
+    }
+}
