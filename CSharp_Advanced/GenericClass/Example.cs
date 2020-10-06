665fbcb6aaa32451836689cda6e2da09b30e80c6
MD Rasel <raselpatenga@gmail.com>
2020-10-06 22:53:34 +0600

    Generic Class

---
+++ CSharp_Advanced/GenericClass/Example.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass
+{
+    class Example<Type>
+    {
+        Type box;
+        public Type data {
+            set
+            {
+                this.box = value;
+            }
+            get
+            {
+                return this.box;
+            }
+        }
+
+        //public Example(Type b)
+        //{
+        //    this.box = b;
+        //}
+        //public Type getBox()
+        //{
+        //    return this.box;
+        //}
+    }
+}
---
+++ CSharp_Advanced/GenericClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example<int> number = new Example<int>();
+            Example<string> name = new Example<string>();
+            number.data = 20;
+            name.data = "Adil Khan";
+
+            Console.WriteLine(number.data);
+            Console.WriteLine(name.data);
+            Console.ReadKey();
+        }
+    }
+}
